c2294784875a07ea59fb9a4ccce72efa409c56be
Kvi666 <kavisl.fantasy@gmail.com>
2024-01-13 02:18:44 +0530

    #

---
+++ index.js
@@ -0,0 +1,8 @@
+// Sticky Navbar
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
