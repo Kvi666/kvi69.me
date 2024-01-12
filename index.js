40647d1541fd87452800f2b3bb14b6fd2f6918be
Kvi666 <kavisl.fantasy@gmail.com>
2024-01-13 02:45:08 +0530

    #

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ $(window).scroll(function () {
       $(".navbar").removeClass("sticky-top shadow-sm");
     }
   });
+
+
+  // typewriter 
+  const phrases = ["I'm a developer", "I'm a designer"];
+  let index = 0;
+  let isForward = true;
+  let charIndex = 0;
+  const speed = 100; // Typing speed in milliseconds
+
+  function typeWriter() {
+    const textElement = document.getElementById('typewriter-text');
+    const currentPhrase = phrases[index];
+
+    if (isForward) {
+      textElement.innerHTML = currentPhrase.substring(0, charIndex++);
+      if (charIndex > currentPhrase.length) {
+        isForward = false;
+        setTimeout(typeWriter, speed * 2); // Pause at the end of forward typing
+      } else {
+        setTimeout(typeWriter, speed);
+      }
+    } else {
+      textElement.innerHTML = currentPhrase.substring(0, charIndex--);
+      if (charIndex === 0) {
+        isForward = true;
+        index = (index + 1) % phrases.length; // Switch to the next phrase
+        setTimeout(typeWriter, speed); // Pause before starting backward typing
+      } else {
+        setTimeout(typeWriter, speed);
+      }
+    }
+  }
+
+  // Start the typewriter effect
+  typeWriter();
