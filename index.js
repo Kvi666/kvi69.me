3966fd62e910ac1ce0931e1f56ad7b2b62cc0c8d
Kvi666 <kavisl.fantasy@gmail.com>
2024-01-17 12:00:52 +0530

    #

--- index.js
+++ index.js
@@ -50,3 +50,21 @@ $(window).scroll(function () {
   type('developer', indexDeveloper);
   type('designer', indexDesigner);
   // typewriter end
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('back-to-top-btn');
+
+    window.addEventListener('scroll', function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+});
