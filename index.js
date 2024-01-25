7e7a3397797218494437e398c17da6b9fe7d9fbc
Kvi666 <kavisl.fantasy@gmail.com>
2024-01-25 14:48:04 +0530

    #

--- index.js
+++ index.js
@@ -83,17 +83,3 @@ $(window).scroll(function () {
     navbarCollapse.classList.remove("show");
   }
     //hamburger close end
-
-
-
-
-
-
-
-    // loader 
-
-    window.addEventListener("load", function () {
-      const loader = document.querySelector(".loader");
-      loader.style.display = "none";
-  });
-  
