b4d4a4209cdc38070cca0f393635b289b95b41da
Kvi666 <kavisl.fantasy@gmail.com>
2024-03-30 15:50:12 +0530

    #

--- index.js
+++ index.js
@@ -1,85 +1,64 @@
 // Sticky Navbar
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 45) {
-      $(".navbar").addClass("sticky-top shadow-sm");
-    } else {
-      $(".navbar").removeClass("sticky-top shadow-sm");
-    }
-  });
-
-
-  // typewriter start
-  const phrases = ["Developer", "Designer", "e-Sport Player"];
-  let indexDeveloper = 0;
-  let indexDesigner = 0;
-
-  function type(id, index) {
-    let charIndex = 0;
-    let isForward = true;
-    let currentText = '';
-
-    function updateText() {
-      currentText = phrases[index].substring(0, charIndex);
-      document.getElementById(id).innerText = currentText;
-    }
-
-    function animate() {
-      if (isForward) {
-        charIndex++;
-      } else {
-        charIndex--;
-      }
-
-      updateText();
-
-      if (isForward && charIndex > phrases[index].length) {
-        isForward = false;
-        setTimeout(animate, 1000);
-      } else if (!isForward && charIndex === 0) {
-        isForward = true;
-        index = (index + 1) % phrases.length;
-        setTimeout(() => type(id, index), 1000);
-      } else {
-        setTimeout(animate, 100);
-      }
-    }
-
-    animate();
+  if ($(this).scrollTop() > 45) {
+    $(".navbar").addClass("sticky-top shadow-sm");
+  } else {
+    $(".navbar").removeClass("sticky-top shadow-sm");
   }
-
-  type('developer', indexDeveloper);
-  type('designer', indexDesigner);
-  // typewriter end
-
-  // back to top btn start
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const backToTopButton = document.getElementById('back-to-top-btn');
-
-    window.addEventListener('scroll', function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    });
-
-    backToTopButton.addEventListener('click', function () {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-    });
 });
 
-  // back to top btn end
 
-  //hamburger close start
-  function toggleNavbar() {
-    var navbarCollapse = document.getElementById("items01");
-    navbarCollapse.classList.toggle("show");
+// typewriter start
+const phrases = ["Developer", "Designer", "e-Sport Player"];
+let indexDeveloper = 0;
+let indexDesigner = 0;
+
+function type(id, index) {
+  let charIndex = 0;
+  let isForward = true;
+  let currentText = '';
+
+  function updateText() {
+    currentText = phrases[index].substring(0, charIndex);
+    document.getElementById(id).innerText = currentText;
   }
 
-  function closeNavbar() {
-    var navbarCollapse = document.getElementById("items01");
-    navbarCollapse.classList.remove("show");
+  function animate() {
+    if (isForward) {
+      charIndex++;
+    } else {
+      charIndex--;
+    }
+
+    updateText();
+
+    if (isForward && charIndex > phrases[index].length) {
+      isForward = false;
+      setTimeout(animate, 1000);
+    } else if (!isForward && charIndex === 0) {
+      isForward = true;
+      index = (index + 1) % phrases.length;
+      setTimeout(() => type(id, index), 1000);
+    } else {
+      setTimeout(animate, 100);
+    }
   }
-    //hamburger close end
+
+  animate();
+}
+
+type('developer', indexDeveloper);
+type('designer', indexDesigner);
+// typewriter end
+
+// //hamburger close start
+// function toggleNavbar() {
+//   var navbarCollapse = document.getElementById("items01");
+//   navbarCollapse.classList.toggle("show");
+// }
+
+// function closeNavbar() {
+//   var navbarCollapse = document.getElementById("items01");
+//   navbarCollapse.classList.remove("show");
+// }
+// //hamburger close end
